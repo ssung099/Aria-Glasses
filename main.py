f2345368bfa947fe363d759d7d1a88c189d8e289
SeungHyun Sung <sshmagic099@gmail.com>
2025-05-12 16:21:30 -0400

    changes

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import pathlib
 import matplotlib.pyplot as plt
 from scipy.signal import stft
 from scipy.io import wavfile
+import os
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
 
 data = pathlib.Path("Audio_Samples")
 output_dir = pathlib.Path("Download")
-check_dir = pathlib.Path("Spectrograms")
+tensor_dir = pathlib.Path("Input")
+
+# Image transformation
+transform_f = transforms.Compose([
+    transforms.Resize((64, 64)),  # Resize all images to 64x64
+    transforms.ToTensor(),       # Convert to tensor and normalize to [0, 1]
+    transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize: mean=0.5, std=0.5
+])
 
 def generate_stft(audio_file):
     relative_path = audio_file.relative_to(data)
-    
-    check_amp = check_dir / f"{relative_path.parent}/{relative_path.stem}_amplitude.png"
-    check_phase = check_dir / f"{relative_path.parent}/{relative_path.stem}_phase.png"
-    
-    if check_amp.exists() and check_phase.exists():
-        # print(f"Skipping {audio_file} as both spectrograms already exist.")
-        return
     
     amplitude_filename = output_dir / f"{relative_path.parent}/{relative_path.stem}_amplitude.png"
     amplitude_filename.parent.mkdir(parents=True, exist_ok=True)
@@ -58,12 +62,46 @@ def generate_stft(audio_file):
     plt.savefig(phase_filename)
     plt.close()
 
-    print(relative_path)
+    # print(relative_path)
 
 if __name__ == "__main__":
     # Iterates through every audio sample we have
-    for f in data.rglob('*'):
-        if f.is_file():
-            file = f.relative_to(data)
-            # print(file)
-            generate_stft(f)
+    for audio_source in data.iterdir():
+        for ir_folder in audio_source.iterdir():
+            angle = int(os.path.basename(ir_folder).split('IR_')[-1])
+
+            tensor_file = tensor_dir / audio_source.name / f"{angle}_tensor.pt"
+
+            if tensor_file.exists():
+                # print(f"Skipping {output_file} as both spectrograms already exist.")
+                continue
+            
+            tensor_file.parent.mkdir(parents=True, exist_ok=True)
+
+            for file in ir_folder.iterdir():
+                generate_stft(file)
+            
+            image_files = sorted([os.path.join(output_dir, audio_source.name, ir_folder.name, file) 
+                      for file in os.listdir(output_dir / audio_source.name / ir_folder.name) 
+                      if file.endswith(".png")])
+            
+            ir_spectrograms = []
+            for image_file in image_files:
+                image = Image.open(image_file).convert("L")  # Convert to grayscale
+                image = transform_f(image)  # Apply transformations
+                ir_spectrograms.append(image)
+            
+            if len(image_files) == 14:  # Ensure all 14 spectrograms are present
+                spectrogram_tensor = torch.stack(ir_spectrograms, dim=0)  # Stack into a single tensor
+                # print(spectrogram_tensor.shape)
+                torch.save((spectrogram_tensor.squeeze()), tensor_file)
+                print(tensor_file)
+            
+            for image_file in image_files:
+                os.remove(image_file)
+        
+        for ir_folder in audio_source.iterdir():
+            for file in ir_folder.iterdir():
+                os.remove(file)
+            os.rmdir(ir_folder)
+        os.rmdir(audio_source)
